362bc15f0e7381df68cd5f67bfea565bd6695e44
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-01-06 06:43:50 +1000

    added charts to the excel which graphs the cumulative gain for each pair and each cross and fixed issues with widths of columns in the excel file

--- simulation/ma_excel.py
+++ simulation/ma_excel.py
@@ -1,5 +1,56 @@
 import pandas as pd
 
+WIDTHS = {
+    'L:L' : 20,
+    'B:I' : 13,
+}
+
+def set_widths(pair, writer):
+    worksheet = writer.sheets[pair]
+    for k, v in WIDTHS.items():
+        worksheet.set_column(k, v)
+
+def get_line_chart(book, start_row, end_row, labels_col, data_col, title, sheetname):
+    chart = book.add_chart({
+        "type": "line",
+    })
+
+    chart.add_series({
+        'categories' : [sheetname, start_row, labels_col, end_row, labels_col],
+        'values' : [sheetname, start_row, data_col, end_row, data_col],
+        'line' : {'color' : 'blue'},
+    })
+
+    chart.set_title({'name' : title})
+    chart.set_legend({'none': True})
+
+    return chart
+
+def add_chart(pair, cross, df, writer):
+    workbook = writer.book
+    worksheet = writer.sheets[pair]
+
+    chart = get_line_chart(workbook, 1, df.shape[0], 11, 12, f"GAIN_C for {pair} {cross}", pair)
+    chart.set_size({'x_scale': 2.5, 'y_scale': 2.5})
+
+    worksheet.insert_chart('O1', chart)
+
+def add_pair_charts(df_ma_res, df_ma_trades, writer):
+    cols = ["time", "GAIN_C"]
+
+    # dataframe with the first row of each pair all together in one table
+    df_temp = df_ma_res.drop_duplicates(subset="pair")
+
+    for _, row in df_temp.iterrows():
+        # Gets the cumulative gain for each pair and its cross
+        dft = df_ma_trades[(df_ma_trades.cross == row.cross)&(df_ma_trades.pair == row.pair)].copy()
+        dft[cols].to_excel(writer, sheet_name=row.pair, index=False, startrow=0, startcol=11)
+
+        set_widths(row.pair, writer)
+
+        # Adds the chart for each pair
+        add_chart(row.pair, row.cross, dft, writer)
+
 def add_pair_sheets(df_ma_res, writer):
     # For each pair in the df_ma_res (i.e. each combination of long and short moving average applied to a trading pair)
     for p in df_ma_res.pair.unique():
@@ -15,6 +66,7 @@ def prepare_data(df_ma_res, df_ma_trades):
 def process_data(df_ma_res, df_ma_trades, writer):
     prepare_data(df_ma_res, df_ma_trades)
     add_pair_sheets(df_ma_res, writer)
+    add_pair_charts(df_ma_res, df_ma_trades, writer)
 
 
 def create_excel(df_ma_res, df_ma_trades, granularity):
