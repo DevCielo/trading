a4e88fecf23b3a1fa7e0a56bb184680c4633c4c8
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-01-09 12:25:02 +1000

    setup multiprocessing to speed up data collection and running simulations for batches of instruments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
 from dateutil import parser
 from infrastructure.collect_data import run_collection
-from simulation.ema_macd import run_ema_macd
+from simulation.ema_macd_mp import run_ema_macd
 
 if __name__ == "__main__":
     # api = OandaApi()
